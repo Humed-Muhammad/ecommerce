3449c6ef2d1a80bd619830016d9e34d2282d7220
Humed-Muhammad <abc45171@gmail.com>
2021-08-10 14:48:13 +0300

    client-state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, lazy, Suspense } from "react";
 import Navbar from "./Components/Navbar.jsx";
-import Home from "./Components/Home.jsx";
-import Cart from "./Components/Cart.jsx";
-import Products from "./Components/Products.jsx";
-import Register from "./Components/users/Register.jsx";
-import Login from "./Components/users/Login.jsx";
 
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-import UserProfile from "./Components/users/UserProfile.jsx";
-import Order from "./Components/users/Order";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+const UserProfile = lazy(() => import("./Components/users/UserProfile.jsx"));
+const Order = lazy(() => import("./Components/users/Order"));
+
+const Home = lazy(() => import("./Components/Home.jsx"));
+const Cart = lazy(() => import("./Components/Cart.jsx"));
+const Products = lazy(() => import("./Components/Products.jsx"));
+const Register = lazy(() => import("./Components/users/Register.jsx"));
+const Login = lazy(() => import("./Components/users/Login.jsx"));
 
 function App() {
   let [path, setPath] = useState("");
@@ -25,29 +31,33 @@ function App() {
     <div className="bg-gray-white flex flex-col justify-center items-center ">
       <Router className="flex flex-col justify-center items-center ">
         <Navbar />
-        <Route path="/order">
-          {loggedInStatus ? null : <Redirect path="/" />}
-          <Order />
-        </Route>
-        <Route path="/profile">
-          {loggedInStatus ? <UserProfile /> : <Redirect path="/" />}
-        </Route>
-        <Route path="/cart">
-          {loggedInStatus ? null : <Redirect path="/" />}
-          <Cart />
-        </Route>
-        <Route path={`/products/${path}`}>
-          <Products path={path} />
-        </Route>
-        <Route exact path="/">
-          <Home setPath={setPath} />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/login">
-          {loggedInStatus ? <Redirect path="/" /> : <Login />}
-        </Route>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path="/order">
+              {loggedInStatus ? null : <Redirect to="/" />}
+              <Order />
+            </Route>
+            <Route path="/profile">
+              {loggedInStatus ? <UserProfile /> : <Redirect to="/" />}
+            </Route>
+            <Route path="/cart">
+              {loggedInStatus ? null : <Redirect to="/" />}
+              <Cart />
+            </Route>
+            <Route path={`/products/${path}`}>
+              <Products path={path} />
+            </Route>
+            <Route exact path="/">
+              <Home setPath={setPath} />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/login">
+              {loggedInStatus ? <Redirect to="/" /> : <Login />}
+            </Route>
+          </Switch>
+        </Suspense>
       </Router>
     </div>
   );
